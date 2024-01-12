f22de5701a9607164f0722327656ed06d5673a60
Livi-96 <Olivia.1996@outlook.com>
2024-01-12 09:29:52 +0000

    updated CV

--- mywebsite/src/app/cv/page.jsx
+++ mywebsite/src/app/cv/page.jsx
@@ -1,5 +1,9 @@
 
+import { Document, Page } from 'react-pdf';
+
 export default function CV(){
+
+    const pdfURL = '/assets/CV2024.pdf';
 
     return(<>
     <iframe src="/assets/CV2024.pdf" style={{width: "100%", height: "100vh"}}/>
