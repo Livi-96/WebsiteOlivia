bf448580c8d7385b4d5341b326d87b3c18484747
Livi-96 <Olivia.1996@outlook.com>
2024-01-12 10:33:43 +0000

    updated cv phone numner

--- mywebsite/src/app/page.js
+++ mywebsite/src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <main className={js.className}>
      <Header />
-     {/* <iframe style={{ borderRadius: "12px", width: "100%", height: "352", frameBorder: "0", allowfullscreen: "", allow: "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture", loading:"lazy"}} src="https://open.spotify.com/embed/track/2F7dO7OCqrxff66epaNPEU?utm_source=generator" /> */}
+
      <section id="about" ><About/> </section>
      <section id="work" > <Work /></section>
      <section id="contact" > <Contact /></section>
